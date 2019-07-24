d015dec4492f3ec5dc9b4f2e1cf322c236ae9f43
Jomon Johnson <jomon.entero@gmail.com>
2019-07-24 14:35:04 +0530

    init

--- src/widgets/DateRangePicker.php
+++ src/widgets/DateRangePicker.php
@@ -15,6 +15,7 @@ class DateRangePicker extends \jino5577\daterangepicker\DateRangePicker
                     'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
                     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'Last 6 Months': [moment().subtract(6, 'month'), moment()],
                     'This Month': [moment().startOf('month'), moment().endOf('month')],
                     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
                 }
