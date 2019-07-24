8dc467d077755057aa9a84e61d1e4d69f5b935b0
Jomon Johnson <jomon.entero@gmail.com>
2019-07-24 12:35:28 +0530

    init

---
+++ src/widgets/DateRangePicker.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace codexten\yii\widgets;
+
+use yii\web\JsExpression;
+
+class DateRangePicker extends \jino5577\daterangepicker\DateRangePicker
+{
+    public function init()
+    {
+        $this->pluginOptions = [
+            'ranges' => new JsExpression("
+                {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                }
+                    "),
+            'autoUpdateInput' => false,
+            'locale' => [
+                'cancelLabel' => 'Clear',
+            ],
+        ];
+        $this->template = '
+            <div class="input-group">
+                <span class="input-group-addon">
+                <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+                {input}
+            </div>
+        ';
+        parent::init();
+    }
+
+}
